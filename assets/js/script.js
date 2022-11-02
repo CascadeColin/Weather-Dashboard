665f72a17abbebfa48975d93a46e8a752398a0d5
Colin Marshall <colin.marshall94@gmail.com>
2022-11-01 17:56:16 -0700

    init commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 // API KEY for weather app = 28c84dca0408e2e881c2b1eec812a185
+"use strict"
+
+/* Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate. */
+
+const APIKEY = '28c84dca0408e2e881c2b1eec812a185';
+// const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKEY;
+// user-defined city selection
+let city;
+
+undefined = 10;
+
+// fetch(QUERYURL)
