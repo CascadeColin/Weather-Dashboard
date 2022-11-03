b3952632748c239ab4763a6b6232d81c86fd2f81
Colin Marshall <colin.marshall94@gmail.com>
2022-11-03 13:50:54 -0700

    built basic html and css structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@
 
 /* Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate. */
 
+let city = 'Seattle'
 const APIKEY = '28c84dca0408e2e881c2b1eec812a185';
-// const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKEY;
+const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKEY;
 // user-defined city selection
-let city;
 
-undefined = 10;
-
-// fetch(QUERYURL)
+fetch(QUERYURL)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => console.log(data))
