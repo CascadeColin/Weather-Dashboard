22e82325bf9554c5413ef5b2766ee280d9ed6976
Colin Marshall <colin.marshall94@gmail.com>
2022-11-03 15:02:59 -0700

    created 2nd html page, updated structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,39 @@
 
 /* Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate. */
 
-let city = 'Seattle'
+let city;
 const APIKEY = '28c84dca0408e2e881c2b1eec812a185';
 const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKEY;
 // user-defined city selection
 
-fetch(QUERYURL)
+
+/* fetch(QUERYURL)
     .then((response) => {
         return response.json()
     })
     .then((data) => console.log(data))
+*/
+
+/* 
+////////////////////////////////////////////
+///////////////   TO-DO   //////////////////
+////////////////////////////////////////////
+
+1) Add city names to localStorage and display as a button
+*/
+searchBtn = document.querySelectorAll("#searchBtn");
+/*
+2) Connect fetch request from weather API
+3) Connect requested API data to the page
+4) Make the weather cards pretty
+
+////////////////////////////////////////////
+///////////////   CRITERIA   ///////////////
+////////////////////////////////////////////
+
+WHEN the user enters a city and clicks search
+THEN a fetch request is made to weather API with defined var 'city'
+THEN a new page is opened using page2.html wireframe
+THEN information is dynically displayed on the page using response from weather API
+THEN var 'city' is stored in local storage with a new button generated so the user can easily access that city again
+*/
