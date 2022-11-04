768867182c7054fa8762dc8b104d8fa52f35bd10
Colin Marshall <colin.marshall94@gmail.com>
2022-11-03 17:20:42 -0700

    added ability to save previously search cities to localstoage and display as button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@
 /* Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate. */
 
 let city;
-const APIKEY = '28c84dca0408e2e881c2b1eec812a185';
-const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKEY;
+// const APIKEY = '28c84dca0408e2e881c2b1eec812a185';
+// const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKEY;
 // user-defined city selection
 
 
@@ -23,7 +23,40 @@ const QUERYURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '
 
 1) Add city names to localStorage and display as a button
 */
-searchBtn = document.querySelectorAll("#searchBtn");
+let searchBtnEl = document.querySelector("#searchBtn");
+let inputEl = document.querySelector("input");
+let cityArr = [];
+let numberOfCities = 0;
+
+searchBtnEl.addEventListener("click", e => {
+    e.preventDefault();
+    numberOfCities++;
+    city = inputEl.value;
+    // cityArr.push(city);
+    // console.log(cityArr);
+    localStorage.setItem(city, city)
+})
+
+function init() {
+    for (let i=0; i<localStorage.length; i++) {
+        // console.log(typeof localStorage.key(i))
+        city = localStorage.key(i)
+        console.log(city)
+        let newCityBtn = document.createElement("button");
+        newCityBtn.innerHTML = city;
+        newCityBtn.style.color = "black";
+        newCityBtn.classList.add("newCityBtn" + numberOfCities);
+        document.getElementById("leftSide").appendChild(newCityBtn);
+    }
+}
+init();
+
+// let newCityBtn = document.createElement("button");
+//     newCityBtn.innerHTML = city;
+//     newCityBtn.style.color = "black";
+//     newCityBtn.classList.add("newCityBtn" + numberOfCities);
+//     document.getElementById("leftSide").appendChild(newCityBtn);
+
 /*
 2) Connect fetch request from weather API
 3) Connect requested API data to the page
