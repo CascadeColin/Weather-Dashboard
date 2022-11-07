e6a1de254fb7ce360f04796d25dfd3841d7bcc02
Colin Marshall <colin.marshall94@gmail.com>
2022-11-06 22:14:56 -0800

    added full API functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,199 @@
-//found strict mode watching Web Dev Simplified and it seems to make debugging MUCH better
-"use strict"
+//found strict mode watching Web Dev Simplified and it seems to make debugging better
+"use strict";
+
 
 let city = "";
 let searchBtnEl = document.querySelector("#searchBtn");
+let newCityBtnEl = document.querySelector(".newCityBtn");
 let inputEl = document.querySelector("input");
 let todayCity = document.querySelector("#todayCity");
 let todayCloud = document.querySelector("#todayCloud");
 let todayTemp = document.querySelector("#todayTemp");
 let todayWind = document.querySelector("#todayWind");
 let todayHumidity = document.querySelector("#todayHumidity");
-const APIKEY = '28c84dca0408e2e881c2b1eec812a185';
+const APIKEY = "28c84dca0408e2e881c2b1eec812a185";
+let dayOneEl = document.getElementById("dayOneEl");
+let dayTwoEl = document.getElementById("dayTwoEl");
+let dayThreeEl = document.getElementById("dayThreeEl");
+let dayFourEl = document.getElementById("dayFourEl");
+let dayFiveEl = document.getElementById("dayFiveEl");
+let icon1 = document.getElementById("icon1");
+let icon2 = document.getElementById("icon2");
+let icon3 = document.getElementById("icon3");
+let icon4 = document.getElementById("icon4");
+let icon5 = document.getElementById("icon5");
 
-/* Use the 5 Day Weather Forecast to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate. */
-
-
-/* 
-////////////////////////////////////////////
-///////////////   TO-DO   //////////////////
-////////////////////////////////////////////
-
-1) Add city names to localStorage and display as a button
-*/
-
-searchBtnEl.addEventListener("click", e => {
-    e.preventDefault();
-    //checks for empty searches
-    if (inputEl.value != "") {
-        city = inputEl.value;
-        localStorage.setItem(city, city);
-    }
-
-    let todayQuery = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&cnt=5&appid=28c84dca0408e2e881c2b1eec812a185&units=imperial';
-    console.log(todayQuery);
-    fetch(todayQuery)
+searchBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  //checks for empty searches
+  if (inputEl.value == "") {
+    return;
+  }
+  city = inputEl.value;
+  localStorage.setItem(city, city);
+  let todayQuery =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&cnt=5&appid=28c84dca0408e2e881c2b1eec812a185&units=imperial";
+  console.log(todayQuery);
+  fetch(todayQuery)
     .then((response) => {
-        return response.json()
+      return response.json();
     })
-    // FIXME: COMPLETE: commented out to save on API reqs
-    /*
     .then((today) => {
-        todayCityDate.textContent = today.name + ",    " + moment.unix(today.dt).format("MMM D, YYYY");
-        // .moment().format("MMM DD, YYYY");
-        todayTemp.textContent = "Temp: " + Math.round(today.main.temp) + " °F";
-        todayCloud.src="https://openweathermap.org/img/wn/" + today.weather[0].icon + ".png";
-        todayCloud.alt=today.weather[0].description;
-        todayWind.textContent = "Wind: " + Math.round(today.wind.speed) + " MPH";
-        todayHumidity.textContent = "Humidity: " + today.main.humidity + " %";
-    })
-    */
-/*
-    FIXME: plug API data into DOM, add date with moment.js
-    let fiveDayQuery = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=28c84dca0408e2e881c2b1eec812a185&units=imperial';
-    console.log(fiveDayQuery);
-    fetch(fiveDayQuery)
+      todayCityDate.textContent =
+        today.name +
+        ",    " +
+        moment.unix(today.dt).format("MMM D, YYYY");
+      todayTemp.textContent = "Temp: " + Math.round(today.main.temp) + " °F";
+      todayCloud.src =
+        "https://openweathermap.org/img/wn/" + today.weather[0].icon + ".png";
+      todayCloud.alt = today.weather[0].description;
+      todayWind.textContent = "Wind: " + Math.round(today.wind.speed) + " MPH";
+      todayHumidity.textContent = "Humidity: " + today.main.humidity + " %";
+    });
+  let fiveDayQuery =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=28c84dca0408e2e881c2b1eec812a185&units=imperial";
+  console.log(fiveDayQuery);
+  fetch(fiveDayQuery)
     .then((response) => {
-        return response.json()
+      return response.json();
     })
-    .then((five) => console.log(five))
-*/
+    .then((data) => {
+      //DAY 1
+      dayOneEl.children[0].textContent =
+        data.city.name +
+        ", " +
+        moment.unix(data.list[0].dt).format("MMM D, YYYY");
+      icon1.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          data.list[0].weather[0].icon +
+          ".png"
+      );
+      icon1.setAttribute("alt", data.list[0].weather[0].description);
+      dayOneEl.children[2].textContent =
+        "Temp: " + Math.round(data.list[0].main.temp) + " °F";
+      dayOneEl.children[3].textContent =
+        "Wind: " + Math.round(data.list[0].wind.speed) + " MPH";
+      dayOneEl.children[4].textContent =
+        "Humidity: " + data.list[0].main.humidity + " %";
+      //DAY 2
+      dayTwoEl.children[0].textContent =
+        data.city.name +
+        ", " +
+        moment.unix(data.list[8].dt).format("MMM D, YYYY");
+      icon2.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          data.list[8].weather[0].icon +
+          ".png"
+      );
+      icon2.setAttribute("alt", data.list[8].weather[0].description);
+      dayTwoEl.children[2].textContent =
+        "Temp: " + Math.round(data.list[8].main.temp) + " °F";
+      dayTwoEl.children[3].textContent =
+        "Wind: " + Math.round(data.list[8].wind.speed) + " MPH";
+      dayTwoEl.children[4].textContent =
+        "Humidity: " + data.list[8].main.humidity + " %";
+      //DAY 3
+      dayThreeEl.children[0].textContent =
+        data.city.name +
+        ", " +
+        moment.unix(data.list[16].dt).format("MMM D, YYYY");
+      icon3.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          data.list[16].weather[0].icon +
+          ".png"
+      );
+      icon3.setAttribute("alt", data.list[16].weather[0].description);
+      dayThreeEl.children[2].textContent =
+        "Temp: " + Math.round(data.list[16].main.temp) + " °F";
+      dayThreeEl.children[3].textContent =
+        "Wind: " + Math.round(data.list[16].wind.speed) + " MPH";
+      dayThreeEl.children[4].textContent =
+        "Humidity: " + data.list[16].main.humidity + " %";
+      //DAY 4
+      dayFourEl.children[0].textContent =
+        data.city.name +
+        ", " +
+        moment.unix(data.list[24].dt).format("MMM D, YYYY");
+      icon4.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          data.list[24].weather[0].icon +
+          ".png"
+      );
+      icon4.setAttribute("alt", data.list[24].weather[0].description);
+      dayFourEl.children[2].textContent =
+        "Temp: " + Math.round(data.list[24].main.temp) + " °F";
+      dayFourEl.children[3].textContent =
+        "Wind: " + Math.round(data.list[24].wind.speed) + " MPH";
+      dayFourEl.children[4].textContent =
+        "Humidity: " + data.list[24].main.humidity + " %";
+      //DAY 5
+      dayFiveEl.children[0].textContent =
+        data.city.name +
+        ", " +
+        moment.unix(data.list[32].dt).format("MMM D, YYYY");
+      icon5.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" +
+          data.list[32].weather[0].icon +
+          ".png"
+      );
+      icon5.setAttribute("alt", data.list[32].weather[0].description);
+      dayFiveEl.children[2].textContent =
+        "Temp: " + Math.round(data.list[32].main.temp) + " °F";
+      dayFiveEl.children[3].textContent =
+        "Wind: " + Math.round(data.list[32].wind.speed) + " MPH";
+      dayFiveEl.children[4].textContent =
+        "Humidity: " + data.list[32].main.humidity + " %";
+    });
+  createSearchTags();
 })
 
+//produces new city button and saves to localStorage
+function createSearchTags() {
+  city = inputEl.value;
+  localStorage.setItem(city, city);
+  let newCityBtn = document.createElement("button");
+  newCityBtn.innerHTML = city;
+  newCityBtn.style.color = "black";
+  newCityBtn.style.width = "100%";
+  newCityBtn.style.fontSize = "1rem";
+  newCityBtn.classList.add("newCityBtn");
+  document.getElementById("leftSide").appendChild(newCityBtn);
+}
 
+//FIXME: not working at all
+//maybe set n=1 to skip search button and run it for any button?
+function searchTags() {
+  for (let n = 0; n < newCityBtnEl.length; n++) {
+    newCityBtnEl[n].addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log(newCityBtnEl[n].textContent);
+      citySearch = newCityBtnEl[n].textContent;
+      console.log(citySearch);
+    });
+  }
+}
+
+//calls localStorage so searchTags persist page reload
 function init() {
-    for (let i=0; i<localStorage.length; i++) {
-        // console.log(typeof localStorage.key(i))
-        city = localStorage.key(i)
-        let newCityBtn = document.createElement("button");
-        newCityBtn.innerHTML = city;
-        newCityBtn.style.color = "black";
-        newCityBtn.style.width = "100%";
-        newCityBtn.style.fontSize = "1rem";
-        newCityBtn.classList.add("newCityBtn");
-        document.getElementById("leftSide").appendChild(newCityBtn);
-        
-    }
+  for (let i = 0; i < localStorage.length; i++) {
+    // console.log(typeof localStorage.key(i))
+    city = localStorage.key(i);
+    let newCityBtn = document.createElement("button");
+    newCityBtn.innerHTML = city;
+    newCityBtn.style.color = "black";
+    newCityBtn.style.width = "100%";
+    newCityBtn.style.fontSize = "1rem";
+    newCityBtn.classList.add("newCityBtn");
+    document.getElementById("leftSide").appendChild(newCityBtn);
+  }
 }
 init();
-
-//Grabs the city name from buttons that are created as a result of previous searches
-//TO-DO connect to API to allow these buttons to search and display weather data
-//searchkey: prevSearch
-//FIXME: cannot search results without refreshing page
-let newCityBtnEl = document.querySelectorAll(".newCityBtn");
-for (let i=0; i<newCityBtnEl.length; i++) {
-    newCityBtnEl[i].addEventListener("click", e => {
-        e.preventDefault();
-        // console.log(newCityBtnEl[i].textContent);
-        city = newCityBtnEl[i].textContent;
-        console.log(city);
-    })
-}
-
-/*
-2) Connect fetch request from weather API
-    -connect main search button
-    -connect dynamically created buttons (searchkey: prevSearch)
-3) Connect requested API data to the page
-4) Make the weather cards pretty
-
-////////////////////////////////////////////
-///////////////   CRITERIA   ///////////////
-////////////////////////////////////////////
-
-WHEN the user enters a city and clicks search
-THEN a fetch request is made to weather API with defined var 'city'
-THEN a new page is opened using page2.html wireframe
-THEN information is dynically displayed on the page using response from weather API
-THEN var 'city' is stored in local storage with a new button generated so the user can easily access that city again
-*/
